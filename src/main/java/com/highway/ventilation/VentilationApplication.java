f7eaec85932b7f0b0b68a93a7ce470e0debaffb2
banjinseong <ps123123ps@naver.com>
2024-07-23 14:18:00 +0900

    rtu시리얼 통신

--- src/main/java/com/highway/ventilation/VentilationApplication.java
+++ src/main/java/com/highway/ventilation/VentilationApplication.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @RequiredArgsConstructor
+@EnableScheduling
 public class VentilationApplication implements CommandLineRunner {
 
 
